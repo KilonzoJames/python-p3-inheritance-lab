d996a0c740b5963547987496d030ddfdbc26b917
KilonzoJames <jay.kilonzo@gmail.com>
2023-08-25 13:57:34 +0300

    python-p3-inheritance-lab

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,12 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+    def learn(self, topic):
+        self.knowledge.append(topic)
+
+student = Student("Alice", "Smith")
+student.learn('topic')
+print(student.knowledge) 
